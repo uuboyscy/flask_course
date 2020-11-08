c49600c16e762992cc64c20761da1f8c5fc903cf
Shi,ChengYou <45199828+uuboyscy@users.noreply.github.com>
2020-11-08 15:50:41 +0800

    Update model_with_sqlalchemy.py

--- 05_simpleMVC/model_with_sqlalchemy.py
+++ 05_simpleMVC/model_with_sqlalchemy.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static', static_folder='./static')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:3306/TESTDB'
 db = SQLAlchemy(app)
-
 
 class Staff(db.Model):
     __tablename__ = 'Staff'
@@ -20,3 +19,30 @@ class Staff(db.Model):
 
     def __repr__(self):
         return "<User(name='%s', record='%s'>" % (self.Name, self.RecordDt)
+
+
+@app.route('/show_staff')
+def hello_google():
+    staff_data = [[d.ID, d.Name, d.DeptId, str(d.Age), d.Gender, d.Salary] for d in db.session.query(Staff)]
+    print(staff_data)
+    column = ['ID', 'Name', 'DeptId', 'Age', 'Gender', 'Salary']
+    return render_template('show_staff.html', staff_data=staff_data,
+                                              column=column)
+@app.route('/show_staff2')
+def hello_google2():
+    staff_data = [[d.ID, d.Name, d.DeptId, str(d.Age), d.Gender, d.Salary] for d in Staff.query()]
+    print(staff_data)
+    column = ['ID', 'Name', 'DeptId', 'Age', 'Gender', 'Salary']
+    return render_template('show_staff.html', staff_data=staff_data,
+                                              column=column)
+
+@app.route('/show_staff/<name>')
+def show_staff(name):
+    staff_data = [[d.ID, d.Name, d.DeptId, str(d.Age), d.Gender, d.Salary] for d in db.session.query(Staff).filter(Staff.Name == name)]
+    print(staff_data)
+    column = ['ID', 'Name', 'DeptId', 'Age', 'Gender', 'Salary']
+    return render_template('show_staff.html', staff_data=staff_data,
+                           column=column)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
